3434cab40bffdbd741c963a5ebe8c7ea60fa7da0
HoaNM98 <hoa.ngo@olli-ai.com>
2022-05-11 16:41:32 +0700

    change script create cert

--- cert/create.sh
+++ cert/create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-user=$1
-password=$2
+#user=$1
+#password=$2
 
 # create CA
 #openssl req \
@@ -14,12 +14,12 @@ password=$2
 #  -passout pass:iS0qNliawf
 
 # create truststore
-#keytool -keystore OLLI.trust -alias CARoot -importcert -file ca.crt #pass: bQEq9BdeW
+#keytool -keystore <Company>.trust -alias CARoot -importcert -file ca.crt
 
-# create keystore for user 
-keytool -genkey -keystore ${user}.key -alias ${user} -dname CN=${user} -keyalg RSA -validity 365 -storepass ${password}
-keytool -certreq -keystore ${user}.key -alias ${user} -file ${user}.unsigned.crt -storepass ${password}
-openssl x509 -req -CA $PWD/ca.crt -CAkey $PWD/ca.key -in ${user}.unsigned.crt -out ${user}.crt -days 365 -CAcreateserial -passin pass:iS0qNliawf
-keytool -import -file $PWD/ca.crt -keystore ${user}.key -alias ca -storepass ${password} -noprompt
-keytool -import -file ${user}.crt -keystore ${user}.key -alias ${user} -storepass ${password} -noprompt
-rm -f ${user}.crt ${user}.unsigned.crt
+## create keystore for user 
+#keytool -genkey -keystore ${user}.key -alias ${user} -dname CN=${user} -keyalg RSA -validity 365 -storepass ${password}
+#keytool -certreq -keystore ${user}.key -alias ${user} -file ${user}.unsigned.crt -storepass ${password}
+#openssl x509 -req -CA $PWD/ca.crt -CAkey $PWD/ca.key -in ${user}.unsigned.crt -out ${user}.crt -days 365 -CAcreateserial -passin pass:iS0qNliawf
+#keytool -import -file $PWD/ca.crt -keystore ${user}.key -alias ca -storepass ${password} -noprompt
+#keytool -import -file ${user}.crt -keystore ${user}.key -alias ${user} -storepass ${password} -noprompt
+#rm -f ${user}.crt ${user}.unsigned.crt
